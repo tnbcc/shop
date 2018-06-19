0a1809a9fb2d035244a424e34a81136df0a07d1e
nbc <tniub.cc@gmail.com>
2018-06-19 15:50:47 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::group(['middleware' => 'email_verified'], function() {
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-        Route::get('/test', function() {
 
-        });
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     });
-});
+    });
