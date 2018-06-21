402f4774f29e74788a6be168d59af8599e66cb99
nbc <tniub.cc@gmail.com>
2018-06-21 15:22:08 +0000

    用户界面 商品列表

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 //首页
 
 Route::get('/','PagesController@root')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
