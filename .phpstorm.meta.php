e9e381f83042bfe7e631ced8786233db2081cc33
nbc <tniub.cc@gmail.com>
2018-07-14 17:41:32 +0000

    订单模型

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-07-05 15:40:44.
+    * Generated on 2018-07-14 16:41:34.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
