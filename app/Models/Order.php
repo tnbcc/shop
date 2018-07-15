800e16f35380e8ed8378de2284293e5f594cab8f
nbc <tniub.cc@gmail.com>
2018-07-15 07:21:39 +0000

    创建订单

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -56,25 +56,23 @@ class Order extends Model
     ];
 
     protected $dates = [
-       'paid_at',
+        'paid_at',
     ];
 
     protected static function boot()
     {
         parent::boot();
-        //监听模型创建事件，在写入数据库之前触发
+        // 监听模型创建事件，在写入数据库之前触发
         static::creating(function ($model) {
-            //如果模型的no字段为空
-
+            // 如果模型的 no 字段为空
             if (!$model->no) {
-                //调用 findAvailableNo 生成订单流水号
+                // 调用 findAvailableNo 生成订单流水号
                 $model->no = static::findAvailableNo();
-                //如果生成失败，则终止创建订单
+                // 如果生成失败，则终止创建订单
                 if (!$model->no) {
                     return false;
                 }
             }
-
         });
     }
 
@@ -88,32 +86,20 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
-    public function findAvailableNo()
+    public static function findAvailableNo()
     {
-        //订单流水号前缀
+        // 订单流水号前缀
         $prefix = date('YmdHis');
-
         for ($i = 0; $i < 10; $i++) {
-            //随机生成 6 位的数字
-            $no = $prefix.str_pad(random_int(0,999999),6,'0',STR_PAD_LEFT);
-
-           //判断是否已经存在
-
-            if (!static::query()->where('no',$no)->exists()) {
+            // 随机生成 6 位的数字
+            $no = $prefix.str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+            // 判断是否已经存在
+            if (!static::query()->where('no', $no)->exists()) {
                 return $no;
             }
         }
+        \Log::warning('find order no failed');
 
-
-          \Log::warning('find order no failed');
-
-          return false;
-
-
-
+        return false;
     }
-
-
-
-
 }
